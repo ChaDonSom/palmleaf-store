c8d8cc658b41392d8ebdb32d7e307169f01fd333
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-09-24 17:36:35 -0400

    use / instead of dashboard

--- resources/views/components/input/group.blade.php
+++ resources/views/components/input/group.blade.php
@@ -5,7 +5,7 @@
 ])
 
 <label {{ $attributes->merge(['class' => 'space-y-1']) }}>
-    <div class="font-medium font-sm">
+    <div class="font-medium text-sm">
         {{ $label }} @if ($required)<small class="text-xs text-red-500">&#42;</small>@endif
     </div>
 
